09bd4d31ae9b4cfd3e67e5466344705a90b70771
ElansGrinfogels <s20grinelan@venta.lv>
2023-02-19 15:58:22 +0200

    Initial commit

---
+++ src/components/game.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+
+
+import Board from "./board";
+import calculateWinner  from "./winner";
+
+const Game = () => {
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setXIsNext((step % 2) === 0);
+    };
+
+    const handleClick = (i) => {
+        const newHistory = history.slice(0, stepNumber + 1);
+        const current = newHistory[newHistory.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory(newHistory.concat([{ squares: squares }]));
+        setStepNumber(newHistory.length);
+        setXIsNext(!xIsNext);
+    };
+
+    const current = history[stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+        const desc = move ?
+            'Move nr' + move :
+            'New Game';
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>
+                    {desc}
+                </button>
+            </li>
+        );
+    });
+
+    let status;
+    if (winner) {
+        status = 'Winner is ' + winner + '!';
+    } else {
+        status = 'Next player is ' + (xIsNext ? 'X' : 'O');
+    }
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board squares={current.squares} onClick={i => handleClick(i)}/>
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+};
+
+export default Game;
---
+++ src/components/squares.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Square(props) {
+    return (
+        <button className='squares' onClick={props.onClick}> {props.value} </button>
+    )
+}
